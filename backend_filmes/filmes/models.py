0d585fcb0949bf6fb9f32fb49a23899f76b359ac
André Bacci <andre.bacci@inspira.com.br>
2021-06-07 15:43:35 -0300

    Inicializa API básica para conexão com o backend; cria modelo inicial de usuário

--- backend_filmes/filmes/models.py
+++ backend_filmes/filmes/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+class User(models.Model):
+    username = models.CharField(max_length=200)
+    email = models.CharField(max_length=200)
+    age = models.IntegerField()
+
+    def __str__(self):
+        return self.username
+
 # Create your models here.
---
+++ backend_filmes/filmes/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+from .models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = (
+            'id',
+            'username',
+            'email',
+            'age',
+        )
+        model = User
