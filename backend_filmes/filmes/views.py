71f887595898bec77f7104cc19d2f9db2f2bb245
André Bacci <andre.bacci@inspira.com.br>
2021-07-17 17:14:42 -0300

    atualiza endpoint de recomendações para evitar repetições

--- backend_filmes/filmes/views.py
+++ backend_filmes/filmes/views.py
@@ -1,8 +1,9 @@
 from rest_framework import generics, filters
 import django_filters.rest_framework
 import random
+from datetime import date
 
-from .models import User, Movie, UserRating
+from .models import User, Movie, UserRating, RecommendationHistory
 from .serializers import UserSerializer, MovieSerializer, UserRatingSerializer
 
 
@@ -33,12 +34,31 @@ class UserRecommendations(generics.ListCreateAPIView):
     serializer_class = MovieSerializer
     user_id = None
 
+    def _check_past_recommendations(self, user, today):
+        year = today.year
+        month = today.month
+        day = today.day
+        user_id = user.id
+        past_recommendation = RecommendationHistory.objects.filter(created_at__year=year,
+                                                           created_at__month=month,
+                                                           created_at__day=day,
+                                                           user__id=user_id)
+        return past_recommendation
+
     def get_queryset(self):
         user = User.objects.get(pk=self.kwargs['user_id'])
+        today = date.today()
+        past_recommendation = self._check_past_recommendations(user, today)
+        if len(past_recommendation) > 0:
+            movie_id = past_recommendation.first().movie.id
+            return Movie.objects.filter(id=movie_id)
         streamings = user.streamings.all()
         queryset = Movie.objects.filter(streamings__in=streamings)
         index = random.randint(0, len(queryset)-2)
         queryset = queryset[index:index+1]
+        movie = queryset[0]
+        recommendation = RecommendationHistory(user=user, movie=movie, created_at=today)
+        recommendation.save()
         return queryset
 
 
