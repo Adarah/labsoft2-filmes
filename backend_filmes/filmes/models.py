5fd2298afb63b5b65d86ee9fc707f8101459994f
André Bacci <andre.bacci@inspira.com.br>
2021-07-18 17:48:09 -0300

    Remove campo idade

--- backend_filmes/filmes/models.py
+++ backend_filmes/filmes/models.py
@@ -14,7 +14,6 @@ class User(models.Model):
     id = models.CharField(primary_key=True, null=False, blank=False, max_length=30)
     username = models.CharField(max_length=200, null=False, blank=False)
     email = models.CharField(max_length=200, null=False, blank=False)
-    age = models.IntegerField(null=False, blank=False)
     streamings = models.ManyToManyField(StreamingService, blank=True)
 
     def __str__(self):
